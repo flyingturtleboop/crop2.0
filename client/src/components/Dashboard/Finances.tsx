5bcd05cd0a13914d63c496f8158e7de0644d47e0
flyingturtleboop <praneeths14209@gmail.com>
2025-04-04 19:19:05 -0400

    Update Finances.tsx

--- client/src/components/Dashboard/Finances.tsx
+++ client/src/components/Dashboard/Finances.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Finances.tsx
 import React from 'react';
 import FinanceTable from './FinancesComponents/FinanceTable';
@@ -7,3 +8,15 @@ const Finances: React.FC = () => {
 };
 
 export default Finances;
+=======
+const Finances = () => {
+    return (
+      <div className="bg-white rounded-lg pb-4 shadow p-4">
+        
+      </div>
+    );
+  };
+  
+  export default Finances;
+  
+>>>>>>> f7d65b770927d37c99c9bca1625642f093715e8c
