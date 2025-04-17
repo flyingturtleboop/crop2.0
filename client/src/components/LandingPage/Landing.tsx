ffd6901496631765baf92a2fb725fda9c0bb114a
kpraneeth526 <kpraneeth526@gmail.com>
2025-04-17 11:14:28 -0400

    changes

--- client/src/components/LandingPage/Landing.tsx
+++ client/src/components/LandingPage/Landing.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import AboutSection from "./AboutSection";
-// import other sections as needed, e.g. FeaturesSection, ContactSection, etc.
 
 const Layout: React.FC = () => {
   return (
