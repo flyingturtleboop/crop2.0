016d84106c064e03cf6b1c789ab9cc12102719bf
flyingturtleboop <praneeths14209@gmail.com>
2025-05-19 22:45:46 -0400

    chat

--- client/src/components/Dashboard/Dashboard_main.tsx
+++ client/src/components/Dashboard/Dashboard_main.tsx
@@ -142,13 +142,6 @@ const Dashboard_main: React.FC = () => {
         />
       </main>
 
-      {/* FAQ floating button */}
-      <button
-        onClick={() => setShowChat((v) => !v)}
-        className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition-shadow"
-      >
-        <MessageCircle size={24} />
-      </button>
 
       {showChat && (
         <FAQModal
